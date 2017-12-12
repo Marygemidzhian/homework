a374db67e9bb9bbb83ac82113b89a59643111652
marygemidzhian <mary@cloverpop.com>
2017-12-12 13:35:27 +0200

    second commit

--- my_prog.rb
+++ my_prog.rb
@@ -3,7 +3,7 @@ name = gets.chomp
 puts "Який твій зріст ? "
 height = gets.chomp
 if height.to_i > 110
-puts " #{name}, ваша вага #{height.to_i - 110.to_i }"
+  puts " #{name}, ваша вага #{height.to_i - 110.to_i}"
 elsif height.to_i <= 110
   puts "Ваша вага оптимальна"
 end
